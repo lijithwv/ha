f794207484a9c9e0f9846f164c53735f612879a2
Lijith <lijithwv@gmail.com>
2025-08-21 13:16:32 +1000

    Auto backup 2025-08-21 13:16:32

---
+++ git_backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Navigate to HA config folder
+cd /config || exit 1
+
+# Full path to git
+GIT_BIN=/usr/bin/git
+
+# Stage changes
+$GIT_BIN add .
+
+# Commit only if there are staged changes
+$GIT_BIN commit -m "Auto backup $(date '+%Y-%m-%d %H:%M:%S')"
+
+# Push to remote
+$GIT_BIN push origin main
+
