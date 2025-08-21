cd36783bd5764b8f72f1a287bf12a7ff27989521
Lijith <lijithwv@gmail.com>
2025-08-21 16:14:02 +1000

    Auto backup 2025-08-21 16:14:02

--- git_backup.sh
+++ git_backup.sh
@@ -7,11 +7,11 @@ cd /config
 GIT_BIN=/usr/bin/git
 
 # Stage changes
-$GIT_BIN add .
+git add .
 
 # Commit only if there are staged changes
-$GIT_BIN commit -m "Auto backup $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "Auto backup $(date '+%Y-%m-%d %H:%M:%S')"
 
 # Push to remote
-$GIT_BIN push origin main 2>&1 | tee /config/git_push.log
+git push origin main 2>&1 | tee /config/git_push.log
 
