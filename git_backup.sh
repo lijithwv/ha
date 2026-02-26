fb499311ef07312e86b42189591fae425344a078
Lijith <lijithwv@gmail.com>
2026-02-26 16:27:34 +1100

    Auto backup 2026-02-26 16:27:34

--- git_backup.sh
+++ git_backup.sh
@@ -18,19 +18,20 @@ if git diff --cached --quiet; then
     echo "No changes."
 
     # Still mark run as successful
-    cat <<EOF > "$STATUS_FILE"
-{"last_run": "$NOW","last_success": "$NOW","status": "success","message": "No changes"}
-EOF
-
+#     cat <<EOF > "$STATUS_FILE"
+# {"last_run": "$NOW","last_success": "$NOW","status": "success","message": "No changes"} EOF
+    printf '{"last_run":"%s","last_success":"%s","status":"success","message":"No changes"}' \
+    "$NOW" "$NOW" > "$STATUS_FILE"
 else
     git commit -m "Auto backup $NOW"
     git push origin main
 
     echo "Backup successful"
 
-    cat <<EOF > "$STATUS_FILE"
-{"last_run": "$NOW","last_success": "$NOW","status": "success","message": "Committed and pushed"}
-EOF
+#     cat <<EOF > "$STATUS_FILE"
+# {"last_run": "$NOW","last_success": "$NOW","status": "success","message": "Committed and pushed"} EOF
+    printf '{"last_run":"%s","last_success":"%s","status":"success","message":"Committed and pushed"}' \
+    "$NOW" "$NOW" > "$STATUS_FILE"
 fi
 
 echo "===== Git Backup finished at $(date '+%Y-%m-%d %H:%M:%S') ====="
